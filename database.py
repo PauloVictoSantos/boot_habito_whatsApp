16798b4500404963757c4218c022b1eb8689bf73
Paulo Victor <paulovictorsantosvc@gmail.com>
2025-02-02 14:08:36 -0300

    Adiciona configurações adicionais

--- database.py
+++ database.py
@@ -6,16 +6,8 @@ load_dotenv()
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),       # Obtém o valor da variável DB_HOST
-        user=os.getenv("DB_USER"),       # Obtém o valor da variável DB_USER
-        password=os.getenv("DB_PASSWORD"),  # Obtém o valor da variável DB_PASSWORD
-        database=os.getenv("DB_DATABASE")   # Obtém o valor da variável DB_DATABASE
+        host=os.getenv("DB_HOST"),      
+        user=os.getenv("DB_USER"),      
+        password=os.getenv("DB_PASSWORD"), 
+        database=os.getenv("DB_DATABASE")  
     )
-
-from database import get_db_connection
-
-conn = get_db_connection()
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM atividades")
-result = cursor.fetchall()
-print(result)  # Deve retornar uma lista vazia inicialmente
